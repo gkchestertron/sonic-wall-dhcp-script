0538def54ae740cb1f4b7378d0befc7609e4e3bf
John Fellman <john.fellman@avrs.com>
2014-04-15 11:42:56 -0700

    adds file

---
+++ sonic-wall-script.js
@@ -0,0 +1,7 @@
+// var jq = document.createElement('script');
+// jq.src = "http://github.com/gkchestertron/sonic-wall-dhcp-script";
+// document.getElementsByTagName('head')[0].appendChild(jq);
+
+var jq = document.createElement('script');
+jq.src = "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
