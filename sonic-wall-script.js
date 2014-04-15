2b55e9b4edb887cda6eb3c15e2b5082d4cdf782a
John Fellman <john.fellman@avrs.com>
2014-04-15 11:47:09 -0700

    adds conditional

--- sonic-wall-script.js
+++ sonic-wall-script.js
@@ -1,7 +1,12 @@
-// var jq = document.createElement('script');
-// jq.src = "http://github.com/gkchestertron/sonic-wall-dhcp-script";
-// document.getElementsByTagName('head')[0].appendChild(jq);
-
+//use the follwing snippet to inject this script into a page
+/*
 var jq = document.createElement('script');
-jq.src = "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
+jq.src = "https://raw.githubusercontent.com/gkchestertron/sonic-wall-dhcp-script/master/sonic-wall-script.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
+*/
+
+if (!jQuery) {
+	var jq = document.createElement('script');
+	jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
+	document.getElementsByTagName('head')[0].appendChild(jq);
+}
